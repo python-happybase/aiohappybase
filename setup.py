2afcce1fe0bd99f94ced8adc429dcb3b61c5f575
aiudirog <aiudirog@gmail.com>
2020-01-17 00:09:43 -0500

    Fix version handling
    Signed-off-by: aiudirog <aiudirog@gmail.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
+from pathlib import Path
 from setuptools import setup
 
-setup()
+__version__ = None
+exec(Path('aiohappybase/_version.py').read_text())
+
+setup(
+    version=__version__,
+    install_requires=[
+        line for line in
+        map(str.strip, Path('requirements.txt').read_text().splitlines())
+        if line
+    ],
+)
