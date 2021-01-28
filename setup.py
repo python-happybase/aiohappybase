7411c19451e95b5ac4cb3045c942f97e9125e1a1
Roger Aiudi <aiudirog@gmail.com>
2021-01-28 16:27:24 -0500

    Update tests to run on 3.9
    Signed-off-by: Roger Aiudi <aiudirog@gmail.com>

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     version=versioneer.get_version(),
-    install_requires=[*filter(None, map(str.strip, open('requirements.txt')))],
     cmdclass=versioneer.get_cmdclass(),
+    install_requires=[*filter(None, map(str.strip, open('requirements.txt')))],
 )
